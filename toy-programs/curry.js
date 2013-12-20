8dd623205768b44145599c85025142ac53a92ec6
Tom Shull <shull1@illinois.edu>
2013-12-20 03:28:57 -0600

    exploring array implementations

--- toy-programs/curry.js
+++ toy-programs/curry.js
@@ -6,7 +6,54 @@ Function.method('curry', function (  ) {
     };
 });    // Something isn't right...
 */
+function curry(b) {
+    function specialFunc(c) {
+        return that.apply(null,b,c);
+    }
+    var slice;
+	  var args;
+	  var that;
+	  //slice	= Array.prototype.slice;
+	  //slice	= String.prototype.slice;
+    //args = slice.apply(arguments);
+    that = this;
+    return specialFunc;
+}
+/*
+function curry() {
+    function specialFunc(  ) {
+        return that.apply(null, args.concat(slice.apply(arguments)));
+    }
+    var slice;
+	  var args;
+	  var that;
+	  //slice	= Array.prototype.slice;
+	  slice	= String.prototype.slice;
+    args = slice.apply(arguments);
+    that = this;
+    return specialFunc;
+}
+*/
 
+function add(a, b) {
+    return a + b;
+
+}
+
+var a;
+var a1;
+
+
+Function.prototype.curry =  curry;
+a = add;
+a1 = a.curry("1");
+print(a1("6"));
+/*
+a1 = a.curry(1);
+print(a1(6));
+*/
+
+/*
 Function.prototype.curry = function () {
     var slice = Array.prototype.slice,
         args = slice.apply(arguments),
@@ -21,3 +68,4 @@ var add = function (a, b) {
 };
 var add1 = add.curry(1);
 console.log(add1(6));    // 7
+*/
